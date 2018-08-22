ccde992707c883beb31335adbb22eb07ad88c19c
david nyrnberg <Andennygi@hotmail.com>
2018-08-23 00:44:52 +0200

    removed all matlab files

--- raschModel.m
+++
@@ -1,9 +0,0 @@
-function [raschModel] = raschModel(betaN, deltaI)
-
-% load options
-N = length(betaN);
-I = length(deltaI);
-% prob rasch model form -> 1/(1+exp{deltaI-betaN})
-raschModel = 1./(1+exp(ones(N,1)*deltaI-betaN*ones(1,I))); % TODO: check delta-beta term
-
-end

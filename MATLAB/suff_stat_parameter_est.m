f4ebc3b57b975e78dc04e2771e7267a5ed7f3762
david nyrnberg <Andennygi@hotmail.com>
2018-08-23 00:40:11 +0200

    commiting code

---
+++ MATLAB/suff_stat_parameter_est.m
@@ -0,0 +1,37 @@
+function [delta,beta] = suff_stat_parameter_est(X,lam,epsi,N,D)
+    % sufficient stats
+    xd=sum(X,2);
+    xn=sum(X,1);
+    % initialization
+     bet=0.001*randn(N,1);
+     del=0.001*randn(1,D);
+%         %     
+     w0=[bet;del'];
+     options = optimoptions('fminunc','Algorithm','quasi-newton','SpecifyObjectiveGradient',true,'MaxIter',10^5,'MaxFunEvals',10^5,'TolX',10^-5,'Display','off');
+     % tune parameters
+     w=fminunc(@(w) rasch_negloglik_grad_priv_suff(w,xd,xn,lam),w0,options);
+     bet=w(1:N);
+     del=w((N+1):(N+D))';
+%    % private suff stats
+     xd_priv=max(zeros(size(xd)),xd+(D/epsi)*sign(randn(size(xd))).*log(rand(size(xd))));
+     xd_priv=min(D*ones(size(xd_priv)),xd_priv);
+     xn_priv=max(zeros(size(xn)),xn+(D/epsi)*sign(randn(size(xn))).*log(rand(size(xn))));
+     xn_priv=min(N*ones(size(xn_priv)),xn_priv);
+    % optimize with private suff stats
+    w_priv=fminunc(@(w) rasch_negloglik_grad_priv_suff(w,xd_priv,xn_priv,lam),w0,options);
+    %bet_priv=w(1:N);  % when we compute probabilities this step approximates that each student computes her grade on her own non-private beta
+    del_priv=w_priv((N+1):(N+D))';
+    %now reoptimize beta
+    betn_arr=zeros(N);
+    for q=1:N
+        betn0=0;
+        student=X(q,:);
+        %            [ neglog_cost, neglog_grad  ] = single_beta_suff_stat(betn, sn, del, lam  )
+        betn_arr(q)=fminunc(@(beta) single_beta_TD(beta,del_priv,student,lam),betn0,options);
+    %    sn=xd(q);
+    %    betn_arr(q)=fminunc(@(betn) single_beta_suff_stat(betn, sn, del, lam  ),betn0,options);
+    end
+    delta=del_priv;
+    beta=betn_arr;
+end
+
